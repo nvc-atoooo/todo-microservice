8df8aa2236726f6189fe8de1753c7dd9406b7ac9
nvc-atoooo <cuongnguyen2011.94@gmail.com>
2018-11-15 17:21:27 +0700

    add auth service

--- auth-service/src/main/java/com/example/authservice/AuthSeviceApplication.java
+++ auth-service/src/main/java/com/example/authservice/AuthSeviceApplication.java
@@ -1,9 +1,11 @@
-package com.example.authsevice;
+package com.example.authservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class AuthSeviceApplication {
 
 	public static void main(String[] args) {
