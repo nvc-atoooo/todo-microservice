056c4ea3021aa19fee577db79b42493a9967ca14
nvc-atoooo <cuongnguyen2011.94@gmail.com>
2018-11-29 17:32:01 +0700

    add authorization

---
+++ service/auth-service/src/main/java/com/example/authservice/config/WebSecurityConfig.java
@@ -0,0 +1,31 @@
+package com.example.authservice.config;
+
+import com.example.authservice.service.JdbcUserDetailsService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private JdbcUserDetailsService jdbcUserDetailsService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests().anyRequest().authenticated()
+            .and()
+            .csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(jdbcUserDetailsService)
+            .passwordEncoder(new BCryptPasswordEncoder());
+    }
+}
--- service/auth-service/src/main/java/com/example/authservice/repository/UserRepository.java
+++ service/auth-service/src/main/java/com/example/authservice/repository/UserRepository.java
@@ -2,8 +2,8 @@ package com.example.authservice.repository;
 
 import com.example.authservice.domain.User;
 
-import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+public interface UserRepository extends CrudRepository<User, Long> {
     User findByUsername(String username);
 } 
